e6e3076c2b154dfddabdba6219cea7dbb07e5bf2
dawid <dpilawa@gmail.com>
2015-12-21 15:33:56 +0100

    Removed excess files

--- minix/src/fs/dev.h
+++
@@ -1,9 +0,0 @@
-/* Device table.  This table is indexed by major device number.  It provides
- * the link between major device numbers and the routines that process them.
- */
-
-extern struct dmap {
-  int _PROTOTYPE ((*dmap_opcl), (int, Dev_t, int, int) );
-  void _PROTOTYPE ((*dmap_io), (int, message *) );
-  int dmap_task;
-} dmap[];
